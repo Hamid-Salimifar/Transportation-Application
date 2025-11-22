0da5467f82b4854faab4c8762e2d3650829df48c
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-11-22 22:21:43 +0330

    chore: initialization commit

---
+++ src/test/java/ir/maktabsharif/transportationapplication/TransportationApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.transportationapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
