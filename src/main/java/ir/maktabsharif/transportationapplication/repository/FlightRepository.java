d24a379d7de0d8bc098225c3850680669e63bf05
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-11-24 17:42:35 +0330

    feat:add method for searching by date and destination in FlightRepository

--- src/main/java/ir/maktabsharif/transportationapplication/repository/FlightRepository.java
+++ src/main/java/ir/maktabsharif/transportationapplication/repository/FlightRepository.java
@@ -3,5 +3,9 @@ package ir.maktabsharif.transportationapplication.repository;
 import ir.maktabsharif.transportationapplication.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight,Integer> {
+    List<Flight> findByDateAndDestination(LocalDate date,String destination);
 }
