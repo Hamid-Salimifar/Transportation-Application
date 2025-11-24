861d44cd60be84cd38fcbc856b5b795986f4c12b
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-11-24 15:56:40 +0330

    feat: defined the findByType method in TicketRepository

--- src/main/java/ir/maktabsharif/transportationapplication/repository/TicketRepository.java
+++ src/main/java/ir/maktabsharif/transportationapplication/repository/TicketRepository.java
@@ -3,5 +3,8 @@ package ir.maktabsharif.transportationapplication.repository;
 import ir.maktabsharif.transportationapplication.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket,Integer> {
+    List<Ticket> findByType(String type);
 }
