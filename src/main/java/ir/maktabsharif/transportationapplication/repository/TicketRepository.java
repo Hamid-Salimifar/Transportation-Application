ede82412373a55de3fc7e0a4c12aaad3df6fec65
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-11-24 16:22:01 +0330

    feat:add method for searching by date and destination in TicketRepository

--- src/main/java/ir/maktabsharif/transportationapplication/repository/TicketRepository.java
+++ src/main/java/ir/maktabsharif/transportationapplication/repository/TicketRepository.java
@@ -3,8 +3,10 @@ package ir.maktabsharif.transportationapplication.repository;
 import ir.maktabsharif.transportationapplication.model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket,Integer> {
     List<Ticket> findByType(String type);
+    List<Ticket> findByFlight_DateAndFlight_Destination(LocalDate date,String destination);
 }
