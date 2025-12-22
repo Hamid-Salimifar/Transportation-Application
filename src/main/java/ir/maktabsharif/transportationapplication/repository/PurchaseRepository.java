c0d6d01419f0764d2e379db02571f4474f5c252d
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-22 13:05:38 +0330

    feat (PurchaseRepository): add findByUser_Id method

--- src/main/java/ir/maktabsharif/transportationapplication/repository/PurchaseRepository.java
+++ src/main/java/ir/maktabsharif/transportationapplication/repository/PurchaseRepository.java
@@ -3,5 +3,8 @@ package ir.maktabsharif.transportationapplication.repository;
 import ir.maktabsharif.transportationapplication.model.Purchase;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PurchaseRepository extends JpaRepository<Purchase,Integer> {
+    List<Purchase> findByUser_Id(Integer userID);
 }
