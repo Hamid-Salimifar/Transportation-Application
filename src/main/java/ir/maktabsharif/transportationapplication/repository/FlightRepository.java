5fd41db768880e4876eda161bd77f2e1aa54205a
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-22 13:04:16 +0330

    style (FlightRepository) : fix indentation

--- src/main/java/ir/maktabsharif/transportationapplication/repository/FlightRepository.java
+++ src/main/java/ir/maktabsharif/transportationapplication/repository/FlightRepository.java
@@ -6,10 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
 
-public interface FlightRepository extends JpaRepository<Flight,Integer> {
-    List<Flight> findByDateAndDestination(LocalDate date,String destination);
-    List<Flight> findByEconomyPriceLessThanEqualAndAvailableEconomySeatGreaterThanEqual(double maxPrice,Integer minSeat);
-    List<Flight> findByBusinessPriceLessThanEqualAndAvailableBusinessSeatGreaterThanEqual(double maxPrice,Integer minSeat);
-    List<Flight> findByFirstClassPriceLessThanEqualAndAvailableFirstClassSeatGreaterThanEqual(double maxPrice,Integer minSeat);
+public interface FlightRepository extends JpaRepository<Flight, Integer> {
+    List<Flight> findByDateAndDestination(LocalDate date, String destination);
+
+    List<Flight> findByEconomyPriceLessThanEqualAndAvailableEconomySeatGreaterThanEqual(double maxPrice, Integer minSeat);
+
+    List<Flight> findByBusinessPriceLessThanEqualAndAvailableBusinessSeatGreaterThanEqual(double maxPrice, Integer minSeat);
+
+    List<Flight> findByFirstClassPriceLessThanEqualAndAvailableFirstClassSeatGreaterThanEqual(double maxPrice, Integer minSeat);
 
 }
